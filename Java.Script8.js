3a48057656a35ba229b3dcfb8ca753f9ccaa7581
maksimmironenko <113362779+maksimmironenko@users.noreply.github.com>
2022-12-04 13:59:36 +0300

    Update Java.Script8.js

--- Java.Script8.js
+++ Java.Script8.js
@@ -56,7 +56,7 @@ $(document).ready(function () {
                 message: localStorage.getItem("mes"),
                 name: localStorage.getItem("name")
             },
-            form: "aXHxW8DxN"
+            form:"tQk1Q6yLU"
         }).then(function () {
             alert("Ваше сообщение успешно отправлено!");
         }).catch(function () {
